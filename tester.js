2786361d926d11c4c02daf5459762a51d4e1de90
Luc de Wit <luc.dewit@hotmail.nl>
2021-04-10 22:00:50 +0200

    testing result ui

--- tester.js
+++ tester.js
@@ -1,21 +1,62 @@
 const fs = require("fs");
+const { execSync } = require("child_process");
+const { stderr } = require("process");
+const { fail } = require("assert");
 
-const executers = {
-    "cca": "cca %name% -o output.ccb",
-    "ccvm": "ccvm %name%"
-}
+const productNameMap = {
+    "cca": "CC Assembler"
+};
 
-fs.readdir(__dirname + "/tests/cca", (err, filenames) => {
+let successCount = 0;
+let failCount = 0;
+
+fs.readdir(__dirname + "/tests/", (err, testNames) => {
     if (err) {
         console.log(err);
         return;
     }
-
-    performTests(filenames);
+    
+    performTests(testNames);
 });
 
 const performTests = testNames => {
     testNames.forEach(name => {
-        fs.readdir(`${__dirname}/tests/${name}/inputs`)
+        fs.readdir(`${__dirname}/tests/${name}/inputs/`, (err, filenames) => {
+            if (err) {
+                console.log(err);
+                return;
+            }
+
+            console.log(`───────────── testing ${productNameMap[name]} ────────────\n`);
+            
+            if (name === "cca")
+                filenames.map(performTestCCA)
+
+            console.log(`\n──────────── 🔴 ${failCount} tests failed | 🟢  ${successCount} tests passed ────────────`);
+        });
     });
+};
+
+const performTestCCA = testName => {
+    // compile
+    execSync(`cca ./tests/cca/inputs/${testName} -o ./tests/cca/output.ccb`);
+
+    // get strings based on file name
+    const ccbName = testName.split(".")[0];
+    const testDescription = testName.split(".")[0].split(/(?=[A-Z])/g).join(" ").toLowerCase();
+
+    // read the compiled data and the expected data
+    const compiledData = fs.readFileSync(`./tests/cca/output.ccb`);
+    const expectedData = fs.readFileSync(`./tests/cca/outputs/${ccbName}.ccb`);
+    // console.log(compiledData, expectedData);
+
+    let failed = false;
+
+    if (failed) {
+        ++failCount;
+        console.log(` 🔴  test failed │ ${testDescription}`);
+    } else {
+        ++successCount;
+        console.log(` 🟢  test passed │ ${testDescription}`);
+    }
 };
